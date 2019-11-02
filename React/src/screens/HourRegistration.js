72eb61080889f99417d217f2181d915b5c6c3e02
AlexandraBagi <szandra.bagi@gmail.com>
2019-11-02 16:57:16 +0100

    ButtonVerbose initial implementation

--- React/src/screens/HourRegistration.js
+++ React/src/screens/HourRegistration.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InputField from '../components/InputField'
-import Button from './../components/Button'
+import ButtonVerbose from '../components/ButtonVerbose'
+import Button from '../components/Button'
 
 import Clock from './../images/Clock.PNG'
 // import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
@@ -21,7 +22,7 @@ export default class HourRegistrationScreen extends React.Component {
           <div style={textStyle}>Who are you registering?</div>
           <div className='row'>
             <Button className="buttonStyle" label="Me">{this.props.label}</Button>
-            <Button className="buttonStyle" label="Coworker">{this.props.label}</Button>
+            <ButtonVerbose label="Coworker">{this.props.label}</ButtonVerbose>
             <Button className="buttonStyle" label="Both">{this.props.label}</Button>
           </div>
         </div>
@@ -29,9 +30,9 @@ export default class HourRegistrationScreen extends React.Component {
         <div>
           <div style={textStyle}>Which day are you registering for?</div>
           <div className='row'>
-          <Button className="buttonStyle" label="Today">{this.props.label}</Button>
-          <Button className="buttonStyle" label="Yesterday">{this.props.label}</Button>
-          <Button className="buttonStyle" label="Other">{this.props.label}</Button>
+          <Button label="Today">{this.props.label}</Button>
+          <Button label="Yesterday">{this.props.label}</Button>
+          <ButtonVerbose label="Other">{this.props.label}</ButtonVerbose>
           </div>
       </div>
 
