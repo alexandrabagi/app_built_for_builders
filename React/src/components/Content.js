7bb78105996c3b2804b41e8ce9d4724685e80dd7
AlexandraBagi <szandra.bagi@gmail.com>
2019-11-04 15:57:51 +0100

    Dropdown list started

--- React/src/components/Content.js
+++ React/src/components/Content.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ButtonSubmit from './ButtonSubmit'
+import DropdownMenu from './Dropdown'
 
 export default ({ close }) => (
   <div className="modal">
     <div className="header"> Choose Coworker</div>
-    <div className="content">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-      commodi beatae optio voluptatum sed eius cumque, delectus saepe
-      repudiandae explicabo nemo nam libero ad, doloribus, voluptas rem alias.
-      Vitae?
+    <div className="dropdown">
+        <DropdownMenu />
     </div>
     <button className="close" onClick={close}>Save</button>
   </div>
---
+++ React/src/components/Dropdown.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Dropdown from 'react-dropdown'
+
+const options = [
+    'Anders, Robert', 'Kazmierczak, Leo', 'Muller, Frank', 'Nielsen, Mikkel', 'Sandberg, Rudi'
+]
+
+class DropdownMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: ''
+        }
+        this._onSelect = this._onSelect.bind(this);
+    }
+
+    _onSelect(option) {
+        this.setState({selected: option})
+    }
+
+    render() {
+        const defaultOption = this.state.selected
+        const placeHolderValue = typeof this.state.selected === 'string' ? this.state.selected : this.state.selected.label
+        
+        return(
+            <section>
+                <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Tap to choose you coworker's name" />
+            </section>
+        )
+    }
+}
+
+export default DropdownMenu
