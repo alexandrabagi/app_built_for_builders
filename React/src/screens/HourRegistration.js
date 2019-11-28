d74155c7e69e63727c0f68c2afaeaa75a5f83d13
AlexandraBagi <szandra.bagi@gmail.com>
2019-11-28 11:19:58 +0100

    Both button deleted

--- React/src/screens/HourRegistration.js
+++ React/src/screens/HourRegistration.js
@@ -21,11 +21,15 @@ export default class HourRegistrationScreen extends React.Component {
         hour: ['00', '01', '02', '03','04','05','06','07','08'],
         minute: ['00', '05', '10', '15','20','25','30','35','40', '45','50']
       },
-        selectedButton: null,
+        //selectedButton: null,
+        //buttonsRow1: [
+        //  {id: 0, label: "Me", selected: false},
+        //  {id: 1,label: "Coworker", selected: false, onClick : () => {}},
+        //  {id: 2,label: "Both", selected: false}
+        //],
         buttonsRow1: [
           {id: 0, label: "Me", selected: false},
           {id: 1,label: "Coworker", selected: false, onClick : () => {}},
-          {id: 2,label: "Both", selected: false}
         ],
         buttonsRow2: [
           {id: 3, label: "Today", selected: false},
@@ -80,11 +84,17 @@ export default class HourRegistrationScreen extends React.Component {
 
   changeButtonState (id, index) {
     let buttonState = []
-    if(id<=2) buttonState = this.state.buttonsRow1
+    //if(id<=1) buttonState = this.state.buttonsRow1
     if(id>2) buttonState = this.state.buttonsRow2
     buttonState.map((button) => button.selected = false)
     buttonState[index].selected = !buttonState[index].selected
     this.setState(buttonState)
+  }
+
+  changeSelected = (index) => {
+    let selectedState = this.state.buttonsRow1[index]
+    selectedState.selected = !selectedState.selected
+    this.setState(selectedState)
   }
   
   // Update the timepicker value in response to user picking event
@@ -103,7 +113,6 @@ handleChange = (name, value) => {
         buttonsRow1: [
           {id: 0, label: "Me", selected: false},
           {id: 1,label: "Coworker", selected: false, onClick : () => {}},
-          {id: 2,label: "Both", selected: false}
         ],
         buttonsRow2: [
           {id: 3, label: "Today", selected: false},
@@ -138,7 +147,9 @@ handleChange = (name, value) => {
               id = {this.state.buttonsRow1[0].id} 
               label={this.state.buttonsRow1[0].label} 
               selected={this.state.buttonsRow1[0].selected} 
-              onClick={()=> this.changeButtonState(this.state.buttonsRow1[0].id, 0)}/>
+              //onClick={()=> this.changeButtonState(this.state.buttonsRow1[0].id, 0)}
+              onClick={() => this.changeSelected(0)}
+              />
 
             <Button 
               id={this.state.buttonsRow1[1].id} 
@@ -146,7 +157,7 @@ handleChange = (name, value) => {
               selected={this.state.buttonsRow1[1].selected} 
               onClick={() => {
                 this.openModal()
-                this.changeButtonState(this.state.buttonsRow1[1].id, 1)
+                this.changeSelected(1)
               }}/>
             <Modal 
               show={this.state.showModal}
@@ -154,11 +165,7 @@ handleChange = (name, value) => {
               animation={false} 
               selectedCoworkerM={this.getCoworker}/>
 
-            <Button 
-              id = {this.state.buttonsRow1[2].id} 
-              label={this.state.buttonsRow1[2].label} 
-              selected={this.state.buttonsRow1[2].selected} 
-              onClick={()=> this.changeButtonState(this.state.buttonsRow1[2].id, 2)}/>
+            
         </div>  
       </div>
 
