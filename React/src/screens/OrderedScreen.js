4a4ef88ee72541dc496794ce5b8974b4a898d60e
AlexandraBagi <szandra.bagi@gmail.com>
2019-12-03 23:11:34 +0100

    Merge started, fetching has problems with token

--- React/src/screens/OrderedScreen.js
+++ React/src/screens/OrderedScreen.js
@@ -2,24 +2,47 @@ import React from 'react';
 import data from '../data/fetched'
 
 export default class OrderedScreen extends React.Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true,
-      dataToken: '',
-      dataTokenObject: null,
+      dataToken: null,
+      dataTokenDate: null,
       dataSource: null
     }
   }
-  
+
   componentDidMount() {
-    this.fetchData()
-    /* // fetch access token from API
-    console.log("DataToken before if: " + this.state.dataToken)
-    console.log("DataToken object before if: " + this.state.dataTokenObject)
-    if (this.state.dataTokenObject === null) {
-      return fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/sessions', {
+    var validToken = false;
+    console.log("Datatoken: " + this.state.dataToken)
+    console.log("DataTokenDate: " + this.state.dataTokenDate)
+    if (this.state.dataToken != null && this.state.dataTokenDate != null) {//Check if token is less than 12 min old (server have 15 min span)
+      //https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php
+      var currentTime = new Date()
+      var diff = (currentTime.getTime() - this.state.dataTokenDate.getTime()) / 1000;
+      diff /= 60;
+      var totalMins = Math.abs(Math.round(diff));
+      if (totalMins < 12) {
+        validToken = true;
+      }
+    }
+    console.log("ValidToken: " + validToken)
+    //If token is still valid than go directly to Order() method, otherwise use LogInAndOrder() method to first login and than order
+    if (validToken) {
+      //this.fetchData()
+    }
+    else {
+      console.log("Entered else statement")
+      this.login()
+      this.fetchData()
+    }
+  }
+
+  login = () => {
+    // fetch access token from API
+    console.log("Login called")
+    fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/sessions', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -28,32 +51,31 @@ export default class OrderedScreen extends React.Component {
       },
       body: {}
     })
-    .then ( (response) => response.json() )
-    .then ( (responseJson) => {
-
-      this.setState({
-        isLoading: true,
-        dataToken: responseJson.response.token,
-        dataTokenObject: responseJson.response
+      // The response translated to json
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log("Response" + responseJson)
+        this.setState({
+          isLoading: true,
+          dataToken: responseJson.response.token,
+          dataTokenDate: new Date() //DB do not give us creation date-time, so we create it ourselves here
+        })
       })
-
-    })
-  } else {
-  } */
-    
-    // fetch data
-    //.then (() =>{ 
+      .then(
+        console.log("Datatoken in login: " + this.state.dataToken)
+      )
   }
 
   fetchData = async () => {
     console.log("Fetching data")
+    console.log("DataToken in fetch: " + this.state.dataToken)
     try {
       const response = await fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/layouts/@MatOrd/records', {
         method: 'GET',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer 1d29777820d178c381b4551855ac20351091956d56426ba2d858' //+ this.state.dataToken
+          'Authorization': 'Bearer bf0e9d01a256d1ef1c66ddeb0827efce8a08287f3c3ce1432043' //+ this.state.dataToken
         }
       });
       const responseJson = await response.json();
@@ -71,38 +93,39 @@ export default class OrderedScreen extends React.Component {
 
   renderTableData() {
     return this.state.dataSource.map((item) => {
-       return (
-        <tr key = {item.fieldData.ItemID}>
+      return (
+        <tr key={item.fieldData.ItemID}>
           <td>{item.fieldData.ItemID}</td>
           <td>{item.fieldData.ItemName}</td>
           <td>{item.fieldData.ItemAmount}</td>
         </tr>
-       )
+      )
     })
- }
+  }
 
- renderTableHeader() {
-  let header = ['ItemID', 'ItemName', 'ItemAmount']
-  return header.map((key, index) => {
-     return <th key={index}>{key.toUpperCase()}</th>
-  })
-}
- 
+  renderTableHeader() {
+    let header = ['ItemID', 'ItemName', 'ItemAmount']
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
+
   render() {
-      if (this.state.isLoading === true) {
-        return (
-          <div>Loading...</div>
-        )
-      } else {
-        return (
-          <div id='table_holder'>
+    if (this.state.isLoading === true) {
+      return (
+        <div>Loading...</div>
+      )
+    } else {
+      return (
+        <div id='table_holder'>
           <table id='ordered_items'>
-            <tbody style={{'height': '1140px', 'overflow':'scroll', 'display': 'block'}}>
-                  <tr>{this.renderTableHeader()}</tr>
-                  {this.renderTableData()}
+            <tbody style={{ 'height': '1140px', 'overflow': 'scroll', 'display': 'block' }}>
+              <tr>{this.renderTableHeader()}</tr>
+              {this.renderTableData()}
             </tbody>
           </table>
-          </div> 
-        )}
+        </div>
+      )
     }
+  }
 }
--- React/src/screens/ProductsGridScreen.js
+++ React/src/screens/ProductsGridScreen.js
@@ -11,9 +11,9 @@ class ProductGridScreen extends React.Component {
     //extracting the id from the path and looking up wich material has that id. 
     this.materialId = this.props.materialId
     this.materialsToLookUp = [...this.props.materials];
-    this.selectedMaterial= this.materialsToLookUp.find(material => material.id.toString() === this.materialId);
+    this.selectedMaterial = this.materialsToLookUp.find(material => material.id.toString() === this.materialId);
     //when a material is found, then use the image property for finding the correct json file in the folder structure.
-    this.data = require('../images/'+this.selectedMaterial.image + '/data.json');
+    this.data = require('../images/' + this.selectedMaterial.image + '/data.json');
     this.state = {
       isSaving: false,
       dataToken: null,
@@ -21,27 +21,27 @@ class ProductGridScreen extends React.Component {
       itemToOrder: null
     }
   }
-   
 
-    
+
+
   toggleImageSelect = (id) => {
-      //for now we let the toogleimage method do the same as if you click add
-      let productsToLookUp = [...this.data.products];
-      let selectedItem = productsToLookUp.find(item => item.id === id);
-      selectedItem.count = selectedItem.count + 1 
-      //makes the ui re-render it self
-    }
+    //for now we let the toogleimage method do the same as if you click add
+    let productsToLookUp = [...this.data.products];
+    let selectedItem = productsToLookUp.find(item => item.id === id);
+    selectedItem.count = selectedItem.count + 1
+    //makes the ui re-render it self
+  }
 
   IncrementCount = (item) => {
-      item.count = item.count + 1 
-      //makes the ui re-render it self, so count is updating
+    item.count = item.count + 1
+    //makes the ui re-render it self, so count is updating
     this.setState({
     });
   }
 
-  DecreaseCount =  (item) => {
-    if(item.count > 0)
-      item.count = item.count - 1 
+  DecreaseCount = (item) => {
+    if (item.count > 0)
+      item.count = item.count - 1
     //makes the ui re-render it self, so count is updating
     this.setState({
     });
@@ -49,143 +49,141 @@ class ProductGridScreen extends React.Component {
 
 
   OrderProduct = (item) => {
-      var validToken = false;
-      if(this.state.dataToken != null && this.state.dataTokenDate != null)
-      {//Check if token is less than 12 min old (server have 15 min span)
-        //https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php
-        var currentTime = new Date()
-        var diff =(currentTime.getTime() - this.state.dataTokenDate.getTime()) / 1000;
-        diff /= 60;
-        var totalMins =  Math.abs(Math.round(diff));
-        if(totalMins < 12){
-          validToken = true; 
-        }
+    var validToken = false;
+    if (this.state.dataToken != null && this.state.dataTokenDate != null) {//Check if token is less than 12 min old (server have 15 min span)
+      //https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php
+      var currentTime = new Date()
+      var diff = (currentTime.getTime() - this.state.dataTokenDate.getTime()) / 1000;
+      diff /= 60;
+      var totalMins = Math.abs(Math.round(diff));
+      if (totalMins < 12) {
+        validToken = true;
       }
+    }
 
     //If token is still valid than go directly to Order() method, otherwise use LogInAndOrder() method to first login and than order
-      if(validToken){
-        this.Order(item)
-      }
-      else{
-        this.LogInAndOrder(item);
-      }
+    if (validToken) {
+      this.Order(item)
+    }
+    else {
+      this.LogInAndOrder(item);
+    }
   }
 
- 
-    
   //The method for both: logging in by getting the token and ordering 
-  LogInAndOrder =  (item) => {
-      this.setState({
-        itemToOrder : {
-              fieldData:{
-                ItemID :item.id,
-                ItemName:item.name,
-                ItemAmount: item.count
-              }
-          }
-      });
-
-      return fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/sessions', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic YWRtaW46MzMyMnFqMnM='
+  LogInAndOrder = (item) => {
+    this.setState({
+      itemToOrder: {
+        fieldData: {
+          ItemID: item.id,
+          ItemName: item.name,
+          ItemAmount: item.count
         }
-    })
-      // The response translated to json
-      .then ((response) =>response.json())
-      .then ((responseJson) => {
-       this.setState({
-          isSaving: true,
-          dataToken: responseJson.response.token,
-          dataTokenDate : new Date() //DB do not give us creation date-time, so we create it ourselves here
-        })
+      }
+    });
 
-      })
-      // save data
-      .then (() =>{
-        //turn itemToOrder from this.state into json string as jsonData
-        var jsonData =  JSON.stringify(this.state.itemToOrder)
-        fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/layouts/@MatOrd/records', {
+    return fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/sessions', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.state.dataToken
-        },
-        body: jsonData 
+        'Authorization': 'Basic YWRtaW46MzMyMnFqMnM='
+      }
     })
-  .then ( (response) => response.json() )
-      .then ( (responseJson) => {
-        console.log(responseJson.response);
+      // The response translated to json
+      .then((response) => response.json())
+      .then((responseJson) => {
         this.setState({
-          isSaving: false,
-          //reset the item to order to null; 
-          itemToOrder: null 
+          isSaving: true,
+          dataToken: responseJson.response.token,
+          dataTokenDate: new Date() //DB do not give us creation date-time, so we create it ourselves here
         })
+
       })
+      .then(console.log("DataToken - Product: " + this.state.dataToken))
+      // save data
+      .then(() => {
+        //turn itemToOrder from this.state into json string as jsonData
+        var jsonData = JSON.stringify(this.state.itemToOrder)
+        fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/layouts/@MatOrd/records', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + this.state.dataToken
+          },
+          body: jsonData
+        })
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log(responseJson.response);
+            this.setState({
+              isSaving: false,
+              //reset the item to order to null; 
+              itemToOrder: null
+            })
+          })
       })
-    .catch((error) => {
-      console.log(error)
-    });
-    }
+      .catch((error) => {
+        console.log(error)
+      });
+  }
 
 
 
   //The method for: only ordering
-   Order =  (item) => {
+  Order = (item) => {
 
-     this.state.itemToOrder = {
-              fieldData:{
-                ItemID :item.id,
-                ItemName:item.name,
-                ItemAmount: item.count
-              }
-          }
-      var jsonData =  JSON.stringify(this.state.itemToOrder)
-      return fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/layouts/@MatOrd/records', {
+    this.state.itemToOrder = {
+      fieldData: {
+        ItemID: item.id,
+        ItemName: item.name,
+        ItemAmount: item.count
+      }
+    }
+    var jsonData = JSON.stringify(this.state.itemToOrder)
+    return fetch('https://cloud.protabase.com/fmi/data/vLatest/databases/ByggeBygge_DEV_DATA/layouts/@MatOrd/records', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + this.state.dataToken
-        },
-        body: jsonData 
+      },
+      body: jsonData
     })
-    .then ( (response) => response.json() )
-    .then ( (responseJson) => {
+      .then((response) => response.json())
+      .then((responseJson) => {
         console.log(responseJson.response);
         this.setState({
           isSaving: false,
           //reset the item to order to null; 
-          itemToOrder: null 
+          itemToOrder: null
         })
       })
       .catch((error) => {
-      console.log(error)
+        console.log(error)
       });
-    }
-    
+  }
 
-    render() {
-        return (
-       <div>
-         <div>
-            <h2>{this.selectedMaterial.name}</h2>
-         </div>
-         <ImageGridScreen 
-                  items={this.data.products}
-                  toggleImageSelect = {this.toggleImageSelect}
-                  IncrementCountClick = {this.IncrementCount}
-                  DecreaseCountClick = {this.DecreaseCount}
-                  OrderProductClick = {this.OrderProduct}
-                  showButtons = {true}
-                />
-        
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>{this.selectedMaterial.name}</h2>
         </div>
-      );
-    }
+        <ImageGridScreen
+          items={this.data.products}
+          toggleImageSelect={this.toggleImageSelect}
+          IncrementCountClick={this.IncrementCount}
+          DecreaseCountClick={this.DecreaseCount}
+          OrderProductClick={this.OrderProduct}
+          showButtons={true}
+        />
+
+      </div>
+    );
+  }
 };
 
 export default ProductGridScreen;
